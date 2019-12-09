319bbbf1f29dce7d98b3250642292be6848fcc45
Ryan Angelo <rangelo89@gmail.com>
2019-12-08 19:05:26 -0500

    Adding basic environment for working with light and shadows on objects. Experimenting with the camera and placement of objects.

--- js/cone.js
+++ js/cone.js
@@ -1,40 +1,78 @@
     import * as THREE from '../node_modules/three/build/three.module.js';
-    
+    import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+    import { RectAreaLightUniformsLib } from '../node_modules/three/examples/jsm/lights/RectAreaLightUniformsLib.js';
+
     var camera, scene, renderer;
     var cone;
     var material;
     var geometry;
 
     var targetRotation = 0;
-    var targetRotationOnMouseDown = 0;
-    
-    var windowHalfX = window.innerWidth / 2;
-    var mouseX = 0;
-    var mouseXOnMouseDown = 0;
+    var rectLight;
+    var param = {};
+
+    var origin = new THREE.Vector3();
 
     init();
     animate();
 
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
     document.body.appendChild( renderer.domElement );
 
-    geometry = new THREE.ConeGeometry( 5, 10, 32 );
-    material = new THREE.MeshBasicMaterial( {color: 0x73b504} );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    camera.position.set( 0, 0, 35 );
+
+    var ambient = new THREE.AmbientLight( 0xffffff, 0.1 );
+    scene.add( ambient );
+
+    RectAreaLightUniformsLib.init();
+
+    rectLight = new THREE.RectAreaLight( 0xffffff, 1, 10, 10 );
+    rectLight.position.set( 5, 5, 0 );
+    scene.add( rectLight );
+    var rectLightMesh = new THREE.Mesh( new THREE.PlaneBufferGeometry(), new THREE.MeshBasicMaterial( { side: THREE.BackSide } ) );
+    rectLightMesh.scale.x = rectLight.width;
+    rectLightMesh.scale.y = rectLight.height;
+    rectLight.add( rectLightMesh );
+    var rectLightMeshBack = new THREE.Mesh( new THREE.PlaneBufferGeometry(), new THREE.MeshBasicMaterial( { color: 0x080808 } ) );
+    rectLightMesh.add( rectLightMeshBack );
+
+    geometry = new THREE.ConeGeometry( 10, 10, 32 );
+    material = new THREE.MeshBasicMaterial( {color: 0x0000FF} );
     cone = new THREE.Mesh( geometry, material );
+    cone.castShadow = true;
+    cone.receiveShadow = true;
     scene.add( cone );
 
-    camera.position.z = 5;
+    var matStdObjects = new THREE.MeshStandardMaterial( { color: 0xA00000, roughness: 0, metalness: 0 } );
+    var geoKnot = new THREE.TorusKnotBufferGeometry( 1.5, 0.5, 100, 16 );
+    var mshStdKnot = new THREE.Mesh( geoKnot, matStdObjects );
+    mshStdKnot.position.set( 5, 5, 0 );
+    mshStdKnot.castShadow = true;
+    mshStdKnot.receiveShadow = true;
+    scene.add( mshStdKnot )
 
-    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-    //
-    window.addEventListener( 'resize', onWindowResize, false );
+    param = {
+        motion: true,
+        width: rectLight.width,
+        height: rectLight.height,
+        color: rectLight.color.getHex(),
+        intensity: rectLight.intensity,
+        'ambient': ambient.intensity,
+        'object color': matStdObjects.color.getHex(),
+    };
+
+    var controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.copy( mshStdKnot.position );
+    controls.update();
 }
 
 function onWindowResize() {
@@ -50,43 +88,16 @@ function render() {
 
 function animate() {
     requestAnimationFrame( animate );
-    render();
-}
-
-//
-function onDocumentMouseDown( event ) {
-    event.preventDefault();
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.addEventListener( 'mouseout', onDocumentMouseOut, false );
-    mouseXOnMouseDown = event.clientX - windowHalfX;
-    targetRotationOnMouseDown = targetRotation;
-}
-function onDocumentMouseMove( event ) {
-    mouseX = event.clientX - windowHalfX;
-    targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
-}
-function onDocumentMouseUp() {
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-}
-function onDocumentMouseOut() {
-    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-}
-function onDocumentTouchStart( event ) {
-    if ( event.touches.length == 1 ) {
-        event.preventDefault();
-        mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
-        targetRotationOnMouseDown = targetRotation;
+    if ( param.motion ) {
+        var t = ( Date.now() / 2000 );
+        // move light in circle around center
+        // change light height with sine curve
+        var r = 15.0;
+        var lx = r * Math.cos( t );
+        var lz = r * Math.sin( t );
+        var ly = 5.0 + 5.0 * Math.sin( t / 3.0 );
+        rectLight.position.set( lx, ly, lz );
+        rectLight.lookAt( origin );
     }
-}
-function onDocumentTouchMove( event ) {
-    if ( event.touches.length == 1 ) {
-        event.preventDefault();
-        mouseX = event.touches[ 0 ].pageX - windowHalfX;
-        targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
-    }
+    renderer.render( scene, camera );
 }
